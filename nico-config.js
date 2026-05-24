26640691dbcbd87f88aa2479a4073c0b8b721dfe
Rodrigodanielfranco94-png <rodrigodanielfranco94@gmail.com>
2026-05-23 22:37:44 -0700

    Update nico-config.js

--- nico-config.js
+++ nico-config.js
@@ -1,23 +1,31 @@
-const PENSAR_NICO_URL =
-"https://us-central1-elite-cleaners-app.cloudfunctions.net/pensarNico";
+window.NICO_CONFIG = {
+  PENSAR_NICO_URL:
+    "https://us-central1-elite-cleaners-app.cloudfunctions.net/pensarNico",
 
-const CREAR_ESTIMATE_URL =
-"https://us-central1-elite-cleaners-app.cloudfunctions.net/crearEstimateNico";
+  CREAR_ESTIMATE_URL:
+    "https://us-central1-elite-cleaners-app.cloudfunctions.net/crearEstimateNico",
 
-const CONSULTAR_ESTIMATES_URL =
-"https://us-central1-elite-cleaners-app.cloudfunctions.net/consultarEstimates";
+  CONSULTAR_ESTIMATES_URL:
+    "https://us-central1-elite-cleaners-app.cloudfunctions.net/consultarEstimates",
 
-const CREAR_INVOICE_URL =
-"https://us-central1-elite-cleaners-app.cloudfunctions.net/crearInvoiceNico";
+  CREAR_INVOICE_URL:
+    "https://us-central1-elite-cleaners-app.cloudfunctions.net/crearInvoiceNico",
 
-const CONSULTAR_INVOICES_URL =
-"https://us-central1-elite-cleaners-app.cloudfunctions.net/consultarInvoices";
+  CONSULTAR_INVOICES_URL:
+    "https://us-central1-elite-cleaners-app.cloudfunctions.net/consultarInvoices",
 
-const CREAR_RECEIPT_URL =
-"https://us-central1-elite-cleaners-app.cloudfunctions.net/crearReceiptNico";
+  ENVIAR_CORREO_NICO_URL:
+    "https://us-central1-elite-cleaners-app.cloudfunctions.net/enviarCorreoNico",
 
-const ELITE_REVIEW_LINK =
-"https://maps.app.goo.gl/VZjAsLsYbKYhszx27?g_st=ic";
+  CREAR_RECEIPT_URL:
+    "https://us-central1-elite-cleaners-app.cloudfunctions.net/crearReceiptNico",
 
-const ELITE_BASE_URL =
-"https://rodrigodanielfranco94-png.github.io/elite-cleaners-app";
+  ELITE_LOGO_URL:
+    "assets/Logo.png",
+
+  ELITE_REVIEW_LINK:
+    "https://maps.app.goo.gl/VZjAsLsYbKYhszx27?g_st=ic",
+
+  ELITE_BASE_URL:
+    "https://rodrigodanielfranco94-png.github.io/elite-cleaners-app"
+};
