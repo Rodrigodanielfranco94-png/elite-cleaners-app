c67ccfe893201ede9e5a1a96190fcbfd830a183a
Rodrigodanielfranco94-png <rodrigodanielfranco94@gmail.com>
2026-05-23 21:23:36 -0700

    Create nico-config.js

---
+++ nico-config.js
@@ -0,0 +1,23 @@
+const PENSAR_NICO_URL =
+"https://us-central1-elite-cleaners-app.cloudfunctions.net/pensarNico";
+
+const CREAR_ESTIMATE_URL =
+"https://us-central1-elite-cleaners-app.cloudfunctions.net/crearEstimateNico";
+
+const CONSULTAR_ESTIMATES_URL =
+"https://us-central1-elite-cleaners-app.cloudfunctions.net/consultarEstimates";
+
+const CREAR_INVOICE_URL =
+"https://us-central1-elite-cleaners-app.cloudfunctions.net/crearInvoiceNico";
+
+const CONSULTAR_INVOICES_URL =
+"https://us-central1-elite-cleaners-app.cloudfunctions.net/consultarInvoices";
+
+const CREAR_RECEIPT_URL =
+"https://us-central1-elite-cleaners-app.cloudfunctions.net/crearReceiptNico";
+
+const ELITE_REVIEW_LINK =
+"https://maps.app.goo.gl/VZjAsLsYbKYhszx27?g_st=ic";
+
+const ELITE_BASE_URL =
+"https://rodrigodanielfranco94-png.github.io/elite-cleaners-app";
